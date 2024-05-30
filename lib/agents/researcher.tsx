2a7320adc985f8de03a7d9dbbeccf5a04707532b
your_username <your_email@example.com>
2024-05-30 16:53:30 +0800

    c

--- lib/agents/researcher.tsx
+++ lib/agents/researcher.tsx
@@ -1,10 +1,5 @@
 import { createStreamableUI, createStreamableValue } from 'ai/rsc'
-import {
-  CoreMessage,
-  ToolCallPart,
-  ToolResultPart,
-  streamText as nonexperimental_streamText
-} from 'ai'
+import { CoreMessage, ToolCallPart, ToolResultPart, streamText } from 'ai'
 import { Section } from '@/components/section'
 import { BotMessage } from '@/components/message'
 import { getTools } from './tools'
@@ -12,7 +7,7 @@ import { getModel } from '../utils'
 
 export async function researcher(
   uiStream: ReturnType<typeof createStreamableUI>,
-  streamText: ReturnType<typeof createStreamableValue<string>>,
+  sText: ReturnType<typeof createStreamableValue<string>>,
   messages: CoreMessage[],
   useSpecificModel?: boolean
 ) {
@@ -20,22 +15,22 @@ export async function researcher(
   let hasError = false
   const answerSection = (
     <Section title="Answer">
-      <BotMessage content={streamText.value} />
+      <BotMessage content={sText.value} />
     </Section>
   )
 
   const currentDate = new Date().toLocaleString()
-  const result = await nonexperimental_streamText({
+  const result = await streamText({
     model: getModel(),
     maxTokens: 2500,
-    system: `As a professional search expert, you possess the ability to search for any information on the web.
-    or any information on the web.
-    For each user query, utilize the search results to their fullest potential to provide additional information and assistance in your response.
+    system: `
     If there are any images relevant to your answer, be sure to include them as well.
     Aim to directly address the user's question, augmenting your response with insights gleaned from the search results.
     Whenever quoting or referencing information from a specific URL, always cite the source URL explicitly.
-    The retrieve tool can only be used with URLs provided by the user. URLs from search results cannot be used.
-    Please match the language of the response to the user's language. Current date and time: ${currentDate}`,
+    The retrieve tool can only be used with URLs provided by the user.
+    Please match the language of the response to the user's language. 
+    Limit your response to 200 tokens.
+    Current date and time: ${currentDate}`,
     messages,
     tools: getTools({
       uiStream,
@@ -44,7 +39,7 @@ export async function researcher(
   }).catch(err => {
     hasError = true
     fullResponse = 'Error: ' + err.message
-    streamText.update(fullResponse)
+    sText.update(fullResponse)
   })
 
   // If the result is not available, return an error response
@@ -69,11 +64,16 @@ export async function researcher(
           }
 
           fullResponse += delta.textDelta
-          streamText.update(fullResponse)
+          sText.update(fullResponse)
         }
+
+        console.log('fullResponse', delta)
+
         break
       case 'tool-call':
         toolCalls.push(delta)
+
+        console.log('tool-call', delta)
         break
       case 'tool-result':
         // Append the answer section if the specific model is not used
@@ -84,6 +84,8 @@ export async function researcher(
           hasError = true
         }
         toolResponses.push(delta)
+
+        console.log('tool-result', delta)
         break
       case 'error':
         console.log('Error: ' + delta.error)
@@ -92,15 +94,17 @@ export async function researcher(
         break
     }
   }
+
   messages.push({
     role: 'assistant',
     content: [{ type: 'text', text: fullResponse }, ...toolCalls]
   })
 
   if (toolResponses.length > 0) {
-    // Add tool responses to the messages
     messages.push({ role: 'tool', content: toolResponses })
   }
+
+  console.log('messages', messages)
 
   return { result, fullResponse, hasError, toolResponses }
 }
